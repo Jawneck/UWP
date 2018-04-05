db7c782858fdee164d9ef32fd8e35072c4943bad
Danielis Joniskis <g00333859@gmit.ie>
2018-04-05 17:17:18 +0100

    Added move method

--- UWP/Assets/Player_Move.cs
+++ UWP/Assets/Player_Move.cs
@@ -7,14 +7,36 @@ public class Player_Move : MonoBehaviour {
     public bool facingRight = true;
     public int playerJumpPwr = 400;
     public int playerSpeed = 12;
+    public float X;
 
-	
-	// Update is called once per frame
-	void Update () {
+
+
+    // Update is called once per frame
+    void Update() {
         Move();
-	}
-
-    void Move(){
-          
     }
+
+    void Move() {
+        //Movement on the x axis
+        X = Input.GetAxis("Horizontal");
+        
+        if(X < 0.0f && facingRight == false){
+            Flip();
+        }
+        else if(X < 0.0f && facingRight == true){
+            Flip();
+        }
+
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(X * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void Flip()
+    {
+
+    }
+
+    void Jump(){
+
+    }
+
 }

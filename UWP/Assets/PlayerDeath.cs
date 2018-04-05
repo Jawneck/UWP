a8bff9c28381df9f7a275bdf66bc7a3f110218c7
Danielis Joniskis <g00333859@gmit.ie>
2018-04-05 22:51:35 +0100

    Triggering death

--- UWP/Assets/PlayerDeath.cs
+++ UWP/Assets/PlayerDeath.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class PlayerDeath : MonoBehaviour {
 
+    public bool Death;
+    public int Health;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start (){
+        Death = true;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update (){
+        if (gameObject.transform.position.y < -10){
+            Death = true;
+        }
+            if(Death == true){
+            StartCoroutine("Die");
+            }
 	}
+
+    IEnumerator Die(){
+        yield return new WaitForSeconds(2);
+    }
 }

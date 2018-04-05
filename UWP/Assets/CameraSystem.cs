1deac26ce5985b563685fe71847e3112f44958eb
Danielis Joniskis <g00333859@gmit.ie>
2018-04-05 22:17:16 +0100

    Finished player camera system

--- UWP/Assets/CameraSystem.cs
+++ UWP/Assets/CameraSystem.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class CameraSystem : MonoBehaviour {
 
-    private GameObject player;
+    //Variables used to clamp the camera into position, so it doesnt move off to the side.
+    private GameObject Player;
+    public float xMax;
+    public float xMin;
+    public float yMax;
+    public float yMin;
 
-	// Use this for initialization
-	void Start () {
-		
+    //Use this for initialization
+    void Start () {
+        //Searches the game and finds any GameObject with the Player tag.
+        Player = GameObject.FindGameObjectWithTag("Player");
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	//LateUpdate is called once per frame, at the end of the Update cycle.
+	void LateUpdate () {
+        float x = Mathf.Clamp(Player.transform.position.x, xMin, xMax);
+        float y = Mathf.Clamp(Player.transform.position.y, yMin, yMax);
+        gameObject.transform.position = new Vector3(x, y, gameObject.transform.position.z);//Not changing z axis because game is 2D.
+    }
 }

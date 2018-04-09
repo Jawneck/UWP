4023004f2dd1631d390b3e6d8c88a05fd0a78449
Danielis Joniskis <g00333859@gmit.ie>
2018-04-09 14:27:31 +0100

    Enemy moving left to right upon hit

--- UWP/Assets/EnemyMove.cs
+++ UWP/Assets/EnemyMove.cs
@@ -4,11 +4,28 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour {
 
-    private int X = 1;
+    private int XDirection = 1;
     private int Speed = 2;
 
 	// Update is called once per frame. This method makes the enemy move.
 	void Update () {
-		gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(X, 0) * Speed;
+        //Using a raycast to detect any object that is lying along the path of the ray.
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(XDirection, 0));
+		gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(XDirection, 0) * Speed;
+
+        //If the distance between the raycast and whatever it hits is less than 0.7, then flip.
+        if(hit.distance < 0.7f){
+            Flip();
+        }
 	}
+
+    //Method to move the enemy left to right.
+    void Flip(){
+        if(XDirection > 0){
+            XDirection = -1;
+        }
+        else{
+            XDirection = 1;
+        }
+    }
 }

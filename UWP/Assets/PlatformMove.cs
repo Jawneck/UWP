2d28cf41479b3bb55006d1a38be2922228eff4b8
Danielis Joniskis <g00333859@gmit.ie>
2018-04-09 22:34:11 +0100

    Testing moving platform

--- UWP/Assets/PlatformMove.cs
+++ UWP/Assets/PlatformMove.cs
@@ -4,34 +4,26 @@ using UnityEngine;
 
 public class PlatformMove : MonoBehaviour
 {
-
+    //Constants and Variable
+    public float rightLimit = 5.5f;
+    public float leftLimit = -5.5f;
     private int XDirection = 1;
     private int Speed = 2;
 
     // Update is called once per frame. This method makes the enemy move.
     void Update()
     {
-        //Using a raycast to detect any object that is lying along the path of the ray.
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(XDirection, 0));
-        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(XDirection, 0) * Speed;
-
-        //If the distance between the raycast and whatever it hits is less than 0.7, then flip.
-        if (hit.distance < 3.5f)
-        {
-            Flip();
-        }
-    }
-
-    //Method to move the enemy left to right.
-    void Flip()
-    {
-        if (XDirection > 0)
+        if (transform.position.x > rightLimit)
         {
             XDirection = -1;
         }
-        else
+        else if (transform.position.x < leftLimit)
         {
             XDirection = 1;
         }
+
+        Vector3 Movement = Vector3.right * XDirection * Speed * Time.deltaTime;
+        transform.Translate(Movement);
     }
+    
 }
--- UWP/Assets/Score.cs
+++ UWP/Assets/Score.cs
@@ -1,19 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Score : MonoBehaviour {
 
+    //Constants and Variables
     public float time = 100;
+    public int score = 0;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
 	// Update is called once per frame
 	void Update () {
         //Ticks the time down second by second
         time -= Time.deltaTime;
+
+        //If you run out of time the level restarts
+        if(time < 0.1f){
+            SceneManager.LoadScene("prototype");
+        }
 	}
 }

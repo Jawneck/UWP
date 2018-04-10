19061341fc2edc5cccd0d3da9b7f8361d7fcbffc
Danielis Joniskis <g00333859@gmit.ie>
2018-04-10 18:29:09 +0100

    Closed the file

--- UWP/Assets/Data.cs
+++ UWP/Assets/Data.cs
@@ -10,7 +10,7 @@ public class Data : MonoBehaviour {
     //A static class which can be accessed throughout the entire project.
     public static Data data;
 
-    public int highScore;
+    public int HighScore;
 
     //A method inside Awake. Awake happens before Start.
     void Awake(){
@@ -25,10 +25,20 @@ public class Data : MonoBehaviour {
 
     //A method which saves all the data.
     public void SaveData(){
+        //Creating a binary formatter.
         BinaryFormatter BinForm = new BinaryFormatter();
 
         //persistentDataPath is the data path that stays around when application is updated
         FileStream file = File.Create(Application.persistentDataPath + "/gameInfo.dat"); //Creating game info file.
+
+        //Creating a container for the data.
+        gameData gameData = new gameData();
+        gameData.highscore = HighScore;//Getting HighScore from Data and putting it into the highscore in gameData class.
+
+        //Serializes to a binary file.
+        BinForm.Serialize(file, gameData);
+
+        file.Close();
     }
 
     //A method which loads all the data.
@@ -41,6 +51,6 @@ public class Data : MonoBehaviour {
 [Serializable]
 class gameData{
 
-    public int highScore;
+    public int highscore;
 
 }

2c66839c0d5bce591f09a21f2026d57aabeb1343
Danielis Joniskis <g00333859@gmit.ie>
2018-04-10 16:56:42 +0100

    Added an if-else in Awake method

--- UWP/Assets/Data.cs
+++ UWP/Assets/Data.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 public class Data : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public static Data data;
+
+    public int highScore;
+
+    //A method inside Awake. Awake happens before Start.
+    void Awake(){
+        if(data == null){//if there is no object called data, then dont destroy this game object.
+            DontDestroyOnLoad(gameObject);
+            data = this;//as this will now be the data object.
+        }
+        else if(data != this){//if there is a data object already.
+            Destroy(gameObject);//Destroy it.
+        }
+    }
+
 }

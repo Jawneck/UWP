793cc37b01ab2b2ddeb416c9d1eabcc1821bac1f
Danielis Joniskis <g00333859@gmit.ie>
2018-04-10 13:25:21 +0100

    Displaying time and score in the game UI

--- UWP/Assets/Score.cs
+++ UWP/Assets/Score.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
@@ -9,27 +10,31 @@ public class Score : MonoBehaviour
     //Constants and Variables
     public float time = 100;
     public int score = 0;
+    public GameObject timeUI;
+    public GameObject scoreUI;
 
     // Update is called once per frame
     void Update()
     {
-        //Ticks the time down second by second
+        //Ticks the time down second by second.
         time -= Time.deltaTime;
+        //Displaying the time on the timeUI object ingame.
+        timeUI.gameObject.GetComponent<Text>().text = ("Time Left: " + (int)time);
+        scoreUI.gameObject.GetComponent<Text>().text = ("Score: " + score);
 
         //If you run out of time the level restarts
-        if (time < 0.1f)
-        {
+        if (time < 0.1f){
             SceneManager.LoadScene("prototype");
         }
     }
 
-    //A method to check if the player enters the EndLevel object.
-    void OnCollisionEnter2D(Collider2D col)
+    //A method to check if the player enters the EndLevel object on trigger.
+    void OnTriggerEnter2D(Collider2D col)
     {
         CountScore();
     }
 
-    //A method which totals up the player score.
+    //A method which totals up the player score
     void CountScore(){
         score = score + (int)(time * 10);
     }

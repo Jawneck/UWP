72ca3866c7e2da76d0dc4f85ab1167a035515fd3
Danielis Joniskis <g00333859@gmit.ie>
2018-04-10 12:52:37 +0100

    Added EndLevel object with score count

--- UWP/Assets/Score.cs
+++ UWP/Assets/Score.cs
@@ -3,20 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Score : MonoBehaviour {
+public class Score : MonoBehaviour
+{
 
     //Constants and Variables
     public float time = 100;
     public int score = 0;
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
         //Ticks the time down second by second
         time -= Time.deltaTime;
 
         //If you run out of time the level restarts
-        if(time < 0.1f){
+        if (time < 0.1f)
+        {
             SceneManager.LoadScene("prototype");
         }
-	}
+    }
+
+    //A method to check if the player enters the EndLevel object.
+    void OnCollisionEnter2D(Collider2D col)
+    {
+        CountScore();
+    }
+
+    //A method which totals up the player score.
+    void CountScore(){
+        score = score + (int)(time * 10);
+    }
 }

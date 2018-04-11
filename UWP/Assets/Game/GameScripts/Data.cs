3f09056d26e83c11e861d123189ebd909f25f1ec
Danielis Joniskis <g00333859@gmit.ie>
2018-04-11 21:22:27 +0100

    Re-organised files/folders

---
+++ UWP/Assets/Game/GameScripts/Data.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class Data : MonoBehaviour {
+
+    //A static class which can be accessed throughout the entire project.
+    public static Data data;
+
+    public int HighScore;
+
+    //A method inside Awake. Awake happens before Start.
+    void Awake(){
+        if(data == null){//if there is no object called data, then dont destroy this game object.
+            DontDestroyOnLoad(gameObject);
+            data = this;//as this will now be the data object.
+        }
+        else if(data != this){//if there is a data object already.
+            Destroy(gameObject);//Destroy it.
+        }
+    }
+
+    //A method which saves all the data.
+    public void SaveData(){
+        //Creating a binary formatter.
+        BinaryFormatter BinForm = new BinaryFormatter();
+
+        //persistentDataPath is the data path that stays around when application is updated
+        FileStream file = File.Create(Application.persistentDataPath + "/gameInfo.dat"); //Creating game info file.
+
+        //Creating a container for the data.
+        gameData gameData = new gameData();
+        gameData.highscore = HighScore;//Getting HighScore from Data and putting it into the highscore in gameData class.
+
+        //Serializes to a binary file. (Encrypts data)
+        BinForm.Serialize(file, gameData);
+
+        file.Close();
+    }
+
+    //A method which loads all the data.
+    public void LoadData(){//If the game info game exists, load the data.
+
+        if(File.Exists(Application.persistentDataPath + "/gameInfo.dat")){
+            //Creating a binary formatter.
+            BinaryFormatter BinForm = new BinaryFormatter();
+            //Opening the game info file.
+            FileStream file = File.Open(Application.persistentDataPath + "/gameInfo.dat", FileMode.Open);
+            //Deserializes the binary file. (Decrypts data)
+            gameData gameData = (gameData)BinForm.Deserialize(file);
+
+            file.Close();
+            HighScore = gameData.highscore;
+           
+        }
+    }
+
+}
+
+[Serializable]
+class gameData{
+
+    public static int highscore;
+
+}
---
+++ UWP/Assets/Game/GameScripts/EnemyMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyMove : MonoBehaviour {
+
+    private int XDirection = 1;
+    private int Speed = 2;
+
+	// Update is called once per frame. This method makes the enemy move.
+	void Update () {
+        //Using a raycast to detect any object that is lying along the path of the ray.
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(XDirection, 0));
+		gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(XDirection, 0) * Speed;
+
+        //If the distance between the raycast and whatever it hits is less than 0.7, then flip.
+        if(hit.distance < 0.7f){
+            Flip();
+            if(hit.collider.tag == "Player"){
+                SceneManager.LoadScene("prototype");
+            }
+        }
+	}
+
+    //Method to move the enemy left to right.
+    void Flip(){
+        if(XDirection > 0){
+            XDirection = -1;
+        }
+        else{
+            XDirection = 1;
+        }
+    }
+}
